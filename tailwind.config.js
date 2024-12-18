952d4dd6c7ddf9227f7da9f11aba745ac82e0107
AkshatRaiShrivastava <shrivastavak2005@gmail.com>
2024-12-18 18:28:20 +0530

    converted module to es6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-  theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
-}
+export const darkMode = ["class"];
+export const content = ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"];
+export const theme = {
+	extend: {
+		borderRadius: {
+			lg: 'var(--radius)',
+			md: 'calc(var(--radius) - 2px)',
+			sm: 'calc(var(--radius) - 4px)'
+		},
+		colors: {}
+	}
+};
+export const plugins = [require("tailwindcss-animate")];
